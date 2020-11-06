e09c7fcc5dec54381fb3bdcdd23052cbb9f35c3f
Dawson1709 <73148314+Dawson1709@users.noreply.github.com>
2020-11-06 07:35:09 -0500

    i hit 100 lines for the first time

---
+++ The final 50.py
@@ -0,0 +1,100 @@
+Hello=(¨hi if you read the title you know that i am doing this on my school computer.¨)
+print(Hello)
+print(¨And right now i am really excited that i can get github on here because i cant get python.¨)
+50=(¨i am really trying to hit 50 lines now though¨)
+from PIL import Image
+import requests
+from io import BytesIO
+
+response = requests.get(url)
+img = Image.open(https://www.ctvnews.ca/lifestyle/woman-yelling-at-cat-meme-his-name-is-smudge-he-s-from-ottawa-and-he-hates-salad-1.4691727)
+*imports cat meme*
+print(¨That was a cat meme, which i commented.¨)
+Print(¨you can even cut and pate if you want to see it¨)
+me=((int) 9)
+if me > 1:
+print(¨correct¨)
+#im bored so i will just put another meme.
+from PIL import Image
+import requests
+from io import BytesIO
+
+response = requests.get(url)
+img = Image.open(https://sayingimages.com/funny-birthday-meme/)
+#import birthday meme
+from PIL import Image
+import requests
+from io import BytesIO
+
+response = requests.get(url)
+img = Image.open(https://www.businessinsider.com/memepac-demolishing-trump-tiktok-2020-10)
+#imported trump meme
+if me == 9:
+Print(¨ramen noodles¨)
+#will print ramen noodles
+from PIL import Image
+import requests
+from io import BytesIO
+
+response = requests.get(url)
+img = Image.open(https://www.kuow.org/stories/depression-memes-teenagers)
+#imports meme.
+If me > 8:
+print(¨ayoo memes for dayz¨)
+#will print ayoo memes for dayz
+from PIL import Image
+import requests
+from io import BytesIO
+
+response = requests.get(url)
+img = Image.open(https://www.thespruceeats.com/best-birthday-cake-delivery-services-4795726)
+#imports link to cake recipe.
+if me >= 9:
+print(¨The second 50¨)
+from PIL import Image
+import requests
+from io import BytesIO)
+
+
+response = requests.get(url)
+img = Image.open(https://www.buzzfeednews.com/article/laurenstrapagiel/heres-why-those-bella-poarch-tiktoks-are-so-damn-addictive)
+#Imputs article of bella poach
+like=(¨respect¨)
+Print(like)
+#prints respect
+from PIL import Image
+import requests
+from io import BytesIO)
+
+
+response = requests.get(url)
+img = Image.open(https://www.dailycal.org/2020/11/02/5-hilarious-memes-from-the-2020-election-cycle/) 
+#imputs memes
+from PIL import Image
+import requests
+from io import BytesIO)
+
+
+response = requests.get(url)
+img = Image.open(https://www.nydailynews.com/entertainment/lol-funny-cat-videos-finally-film-festival-article-1.1112078)
+#imputs article about funny cats.
+eek=((int)70)
+oof=((int)100)
+if eek > me:
+print(oof)
+#will print 100
+from PIL import Image
+import requests
+from io import BytesIO)
+
+
+response = requests.get(url)
+img = Image.open(https://worldcatcomedy.com/time-for-super-laugh-best-funny-cat-videos-2020/)
+#imputs cat pic.
+if oof < eek:
+print(me)
+#will not print anything
+if me < oof:
+print(eek)
+#will print 70
+print(¨omg i just hit 100 lines YES¨)
